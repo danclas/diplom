1868a4f350b64bed75c7432f48ce9006a9b1caa3
danclas <danvershinin@gmail.com>
2023-10-03 21:55:34 +0300

    Change to use with annother shape

--- metrics.py
+++ metrics.py
@@ -15,23 +15,23 @@ def change_mask(sample):
     qrs = [[], []]
     t = [[], []]
     for i in range(sample.shape[0] - 1):
-        if sample[i] != 0 and sample[i + 1] == 0:
+        if sample[i] != 1 and sample[i + 1] == 1:
             # start_P
             p[0].append(i)
-        elif sample[i] != 1 and sample[i + 1] == 1:
+        elif sample[i] != 2 and sample[i + 1] == 2:
             # start_QRS
             qrs[0].append(i)
-        elif sample[i] != 2 and sample[i + 1] == 2:
+        elif sample[i] != 3 and sample[i + 1] == 3:
             # start_T
             t[0].append(i)
 
-        if sample[i] == 0 and sample[i + 1] != 0:
+        if sample[i] == 1 and sample[i + 1] != 1:
             # end_P
             p[1].append(i)
-        elif sample[i] == 1 and sample[i + 1] != 1:
+        elif sample[i] == 2 and sample[i + 1] != 2:
             # end_QRS
             qrs[1].append(i)
-        elif sample[i] == 2 and sample[i + 1] != 2:
+        elif sample[i] == 3 and sample[i + 1] != 3:
             # end_T
             t[1].append(i)
     return p, qrs, t
@@ -79,47 +79,45 @@ def statistics(y_true, y_pred):
     df_errors = pd.DataFrame(
         {'start_p': [[]], 'end_p': [[]], 'start_qrs': [[]], 'end_qrs': [[]], 'start_t': [[]], 'end_t': [[]]})
     for j in range(y_pred.shape[0]):
-        sample_true = np.argmax(y_true[j], -1)
-        sample_pred = preproc(np.argmax(y_pred[j], -1))
+        for k in range(1):
+            p1, qrs1, t1 = change_mask(y_pred[j][k])
+            p2, qrs2, t2 = change_mask(y_true[j][k])
 
-        p1, qrs1, t1 = change_mask(sample_pred)
-        p2, qrs2, t2 = change_mask(sample_true)
+            tp, fp, fn, error = comprassion(p1, p2, 0)
+            df_stat.at['tp', 'start_p'] += len(tp)
+            df_stat.at['fp', 'start_p'] += len(fp)
+            df_stat.at['fn', 'start_p'] += len(fn)
+            df_errors.at[0, 'start_p'].extend(error)
 
-        tp, fp, fn, error = comprassion(p1, p2, 0)
-        df_stat.at['tp', 'start_p'] += len(tp)
-        df_stat.at['fp', 'start_p'] += len(fp)
-        df_stat.at['fn', 'start_p'] += len(fn)
-        df_errors.at[0, 'start_p'].extend(error)
+            tp, fp, fn, error = comprassion(p1, p2, 1)
+            df_stat.at['tp', 'end_p'] += len(tp)
+            df_stat.at['fp', 'end_p'] += len(fp)
+            df_stat.at['fn', 'end_p'] += len(fn)
+            df_errors.at[0, 'end_p'].extend(error)
 
-        tp, fp, fn, error = comprassion(p1, p2, 1)
-        df_stat.at['tp', 'end_p'] += len(tp)
-        df_stat.at['fp', 'end_p'] += len(fp)
-        df_stat.at['fn', 'end_p'] += len(fn)
-        df_errors.at[0, 'end_p'].extend(error)
+            tp, fp, fn, error = comprassion(qrs1, qrs2, 0)
+            df_stat.at['tp', 'start_qrs'] += len(tp)
+            df_stat.at['fp', 'start_qrs'] += len(fp)
+            df_stat.at['fn', 'start_qrs'] += len(fn)
+            df_errors.at[0, 'start_qrs'].extend(error)
 
-        tp, fp, fn, error = comprassion(qrs1, qrs2, 0)
-        df_stat.at['tp', 'start_qrs'] += len(tp)
-        df_stat.at['fp', 'start_qrs'] += len(fp)
-        df_stat.at['fn', 'start_qrs'] += len(fn)
-        df_errors.at[0, 'start_qrs'].extend(error)
+            tp, fp, fn, error = comprassion(qrs1, qrs2, 1)
+            df_stat.at['tp', 'end_qrs'] += len(tp)
+            df_stat.at['fp', 'end_qrs'] += len(fp)
+            df_stat.at['fn', 'end_qrs'] += len(fn)
+            df_errors.at[0, 'end_qrs'].extend(error)
 
-        tp, fp, fn, error = comprassion(qrs1, qrs2, 1)
-        df_stat.at['tp', 'end_qrs'] += len(tp)
-        df_stat.at['fp', 'end_qrs'] += len(fp)
-        df_stat.at['fn', 'end_qrs'] += len(fn)
-        df_errors.at[0, 'end_qrs'].extend(error)
+            tp, fp, fn, error = comprassion(t1, t2, 0)
+            df_stat.at['tp', 'start_t'] += len(tp)
+            df_stat.at['fp', 'start_t'] += len(fp)
+            df_stat.at['fn', 'start_t'] += len(fn)
+            df_errors.at[0, 'start_t'].extend(error)
 
-        tp, fp, fn, error = comprassion(t1, t2, 0)
-        df_stat.at['tp', 'start_t'] += len(tp)
-        df_stat.at['fp', 'start_t'] += len(fp)
-        df_stat.at['fn', 'start_t'] += len(fn)
-        df_errors.at[0, 'start_t'].extend(error)
-
-        tp, fp, fn, error = comprassion(t1, t2, 1)
-        df_stat.at['tp', 'end_t'] += len(tp)
-        df_stat.at['fp', 'end_t'] += len(fp)
-        df_stat.at['fn', 'end_t'] += len(fn)
-        df_errors.at[0, 'end_t'].extend(error)
+            tp, fp, fn, error = comprassion(t1, t2, 1)
+            df_stat.at['tp', 'end_t'] += len(tp)
+            df_stat.at['fp', 'end_t'] += len(fp)
+            df_stat.at['fn', 'end_t'] += len(fn)
+            df_errors.at[0, 'end_t'].extend(error)
 
 
     for index in df_res.columns:
@@ -134,7 +132,8 @@ def statistics(y_true, y_pred):
             df_res.at['PPV', index] = df_stat.loc['tp', index] / (df_stat.loc['tp', index] + df_stat.loc['fp', index])
         except:
             df_res.at['PPV', index] = 0
-        df_res.at['F1', index] =  2 * df_res.loc['Se', index] * df_res.loc['PPV', index] / (df_res.loc['Se', index] + df_res.loc['PPV', index])          
+        df_res.at['F1', index] = 2 * df_res.loc['Se', index] * df_res.loc['PPV', index] / (df_res.loc['Se', index] + 
+                                                                                           df_res.loc['PPV', index])
         df_res.at['sigma^2', index] = np.var(df_errors.loc[0, index])
         df_res.at['m', index] = np.mean(df_errors.loc[0, index])
     return df_res
